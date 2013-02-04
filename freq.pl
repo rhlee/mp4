1663593531b66329edc776b170332cc0e4098bb3
Richard H Lee <ricardohenrylee@gmail.com>
2013-02-04 22:03:34 +0000

    xml

--- freq.pl
+++ freq.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use IPC::Open2;
 
-my $pid = open2(my $r, my $w, "file", $ARGV[0]) or die;
+my $pid = open2(my $stdout, my $stdin, "file", $ARGV[0]) or die;
 waitpid($pid, 0);
 die "Can't file " . $ARGV[0] if ($? >> 8) != 0;
-print <$r> . "\n";
+die "Not an XML file" if (<$stdout> !~ /XML/);
