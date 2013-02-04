115a3115a54e5a35b4b5370c666ed8d06c6252ea
Richard H Lee <ricardohenrylee@gmail.com>
2013-02-04 22:08:32 +0000

    dump file

--- freq.pl
+++ freq.pl
@@ -7,3 +7,10 @@ my $pid = open2(my $stdout, my $stdin, "file", $ARGV[0]) or die;
 waitpid($pid, 0);
 die "Can't file " . $ARGV[0] if ($? >> 8) != 0;
 die "Not an XML file" if (<$stdout> !~ /XML/);
+
+my $dts = 0;
+open(my $file, $ARGV[0]) or die "Can't open file";
+while (my $line = <$file>) {
+  print $line;
+}
+close $file;
