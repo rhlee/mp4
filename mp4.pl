113644d3436855914b54cc58f9a87102f658b5d3
Richard H Lee <ricardohenrylee@gmail.com>
2013-02-05 01:13:53 +0000

    new

---
+++ mp4.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use XML::Twig;
+
+my $factor = 4 / 1001;
+
+my $twig = XML::Twig->new(
+  twig_handlers => {
+    NHNTSample => sub {
+      my $element = $_;
+      $element->set_att(DTS => $element->{'att'}->{'DTS'} * $factor);
+      $element->set_att(CTSOffset =>
+          $element->{'att'}->{'CTSOffset'} * $factor)
+        if ($element->att_exists('CTSOffset'));
+    }
+  },
+  pretty_print => 'nice'
+);
+$twig->parsefile($ARGV[0]);
+$twig->print;
