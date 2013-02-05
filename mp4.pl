8dbb255c1c0c63ae36baa0f12c4a1ca34c81343c
Richard H Lee <ricardohenrylee@gmail.com>
2013-02-05 01:19:04 +0000

    final

--- mp4.pl
+++ mp4.pl
@@ -7,12 +7,14 @@ my $factor = 4 / 1001;
 
 my $twig = XML::Twig->new(
   twig_handlers => {
+    NHNTStream => sub {
+      $_->set_att("timeScale", "1000");
+    },
     NHNTSample => sub {
       my $element = $_;
-      $element->set_att(DTS => $element->{'att'}->{'DTS'} * $factor);
-      $element->set_att(CTSOffset =>
-          $element->{'att'}->{'CTSOffset'} * $factor)
-        if ($element->att_exists('CTSOffset'));
+      $element->set_att("DTS",  $element->att("DTS") * $factor);
+      $element->set_att("CTSOffset", $element->att("CTSOffset") * $factor)
+        if ($element->att_exists("CTSOffset"));
     }
   },
   pretty_print => 'nice'
