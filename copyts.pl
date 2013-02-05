bc8acd767091e1cac6ccb26a2aebcf6685c21ed4
Richard H Lee <ricardohenrylee@gmail.com>
2013-02-05 16:59:03 +0000

    temp

--- copyts.pl
+++ copyts.pl
@@ -4,21 +4,38 @@ use warnings;
 use XML::Twig;
 
 my $factor = 4 / 1001;
-my @ptss = ();
 
+my @ptss = ();
 my $twigin = XML::Twig->new(
   twig_handlers => {
     NHNTSample => sub {
       my $element = $_;
       my $pts = $element->att("DTS");
-      $pts += $element->att("DTS") if $element->att_exists("CTSOffset");
+      $pts += $element->att("DTS") if ($element->att_exists("CTSOffset"));
       push(@ptss, $pts);
     }
   }
 );
 $twigin->parsefile($ARGV[0]);
 @ptss = sort {$a <=> $b} @ptss;
-foreach (@ptss) {
-  print $_ . "\n";
-}
-#$twig->print;
+
+my %elements = ();
+my $count = 0;
+my $twigout = XML::Twig->new(
+  twig_handlers => {
+    NHNTSample => sub {
+      my $element = $_;
+      my $dts = $element->att("DTS");
+      my $pts = $dts;
+      
+      
+      if ($element->att_exists("CTSOffset")) {
+        $pts += $element->att("DTS");
+        $elements{$pts} = $element;
+      }
+      $count++;
+    }
+  }
+);
+$twigin->parsefile($ARGV[1]);
+$twigout->print;
